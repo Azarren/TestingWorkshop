0888e33546c16f868a55d3fac8b04df65e07b17f
Azarren <daniellekushnir22@gmail.com>
2024-07-29 16:35:02 +0100

    Completed mandatory fields scenario

---
+++ features/step_definitions/test_mandatory_fields.rb
@@ -0,0 +1,17 @@
+And ('I leave the date for year leave started blank') do
+  expect(page).to have_content 'When does the leave year start?'
+  expect(page).to have_button 'Continue'
+end
+
+And ('I click the continue button') do
+  click_button('Continue')
+end
+
+Then ("I should see an error message 'Please answer this question'") do
+  expect(page).to have_text 'There is a problem'
+  expect(page).to have_text 'Please answer this question'
+end
+
+And ('input fields should be highlighted in red') do
+  expect(page).to have_selector('div[role="alert"]')
+end
