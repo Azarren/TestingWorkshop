c0593ff747d39c0a4016ec3aa4a2d89a86342151
Azarren <daniellekushnir22@gmail.com>
2024-07-29 13:24:33 +0100

    irregular scenario

--- features/step_definitions/test_calculation_updates.rb
+++ features/step_definitions/test_calculation_updates.rb
@@ -1,55 +1,55 @@
-Given('I navigate to the homepage') do
+Given('I navigate to the homepage (updates)') do
   visit 'https://www.gov.uk/calculate-your-holiday-entitlement'
   sleep 1
 end
 
-And('I should see the homepage') do
+And('I should see the homepage (updates)') do
   expect(page).to have_title 'Calculate holiday entitlement - GOV.UK'
   expect(page).to have_css 'h1.govuk-heading-xl'
   expect(page).to have_link 'Start now'
   expect(page).to have_content 'Use this tool to calculate holiday entitlement for:'
 end
 
-When ("I click on the 'Start now' button") do
+When ("I click on the 'Start now' button (updates)") do
   click_link('Start now')
 end
 
-And("I select the option no for working irregular hours") do
+And("I select the option no for working irregular hours (updates)") do
   expect(page).to have_content 'Does the employee work irregular hours or for part of the year?'
   expect(page).to have_button 'Continue'
   choose('response-1', allow_label_click: true)
   click_button('Continue')
 end
 
-And ('I select the option hours worked per week') do
+And ('I select the option hours worked per week (updates)') do
   expect(page).to have_content 'Is the holiday entitlement based on:'
   expect(page).to have_button 'Continue'
   choose('response-1', allow_label_click: true)
   click_button('Continue')
 end
 
-And ('I select the option for a full leave year') do
+And ('I select the option for a full leave year (updates)') do
   expect(page).to have_content 'Do you want to work out holiday:'
   expect(page).to have_button 'Continue'
   choose('response-0', allow_label_click: true)
   click_button('Continue')
 end
 
-And ('I input 37.5 hours worked per week') do
+And ('I input 37.5 hours worked per week (updates)') do
   expect(page).to have_content 'Number of hours worked per week?'
   expect(page).to have_button 'Continue'
   fill_in('response', with: '37.5')
   click_button('Continue')
 end
 
-And ('I input 5 days worked per week') do
+And ('I input 5 days worked per week (updates)') do
   expect(page).to have_content 'Number of days worked per week?'
   expect(page).to have_button 'Continue'
   fill_in('response', with: '5')
   click_button('Continue')
 end
 
-Then ('I should see the correct submitted answers') do
+Then ('I should see the correct submitted answers (updates)') do
   expect(page).to have_text 'No'
   expect(page).to have_text 'hours worked per week'
   expect(page).to have_text 'for a full leave year'
@@ -57,6 +57,6 @@ Then ('I should see the correct submitted answers') do
   expect(page).to have_text '5.0'
 end
 
-And ('I should see the total entitlement hours') do
+And ('I should see the total entitlement hours (updates)') do
   expect(page).to have_content 'The statutory entitlement is 210 hours holiday.'
 end
