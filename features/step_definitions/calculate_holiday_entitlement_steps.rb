e235294f92ab15548dbbb33a43153f3ca2f22cc6
Azarren <daniellekushnir22@gmail.com>
2024-07-30 11:38:02 +0100

    Merged regular and irregular holiday entitlement tests

--- features/step_definitions/calculate_holiday_entitlement_steps.rb
+++ features/step_definitions/calculate_holiday_entitlement_steps.rb
@@ -47,3 +47,18 @@ end
 And ('I should see the total entitlement hours') do
     expect(page).to have_content 'The statutory entitlement is 210 hours holiday.'
 end
+
+And("I select the option yes for working irregular hours") do
+    expect(page).to have_content 'Does the employee work irregular hours or for part of the year?'
+    expect(page).to have_button 'Continue'
+    choose('response-0', allow_label_click: true)
+    click_button('Continue')
+  end
+
+  And ('I input {int} of {int} {int}') do |day, month, year|
+    expect(page).to have_button 'Continue'
+    fill_in('response-0', with: day)
+    fill_in('response-1', with: month)
+    fill_in('response-2', with: year)
+    click_button('Continue')
+  end
